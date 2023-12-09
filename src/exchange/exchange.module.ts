34e0a1dd8cf4b64073c2c72eba9d6a3b5366eca5
Enrique <e.holguin993@gmail.com>
2023-12-09 18:43:18 -0500

    update exchange.module & repository

--- src/exchange/exchange.module.ts
+++ src/exchange/exchange.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExchangeController } from './exchange.controller';
 import { ExchangeService } from './exchange.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExchangeCurrency } from './entities/exchange-currency.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ExchangeCurrency])],
   controllers: [ExchangeController],
   providers: [ExchangeService],
 })
