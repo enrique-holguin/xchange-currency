c3787d337d23bf7525abbc75cfe4d3511d77f0a5
enrique-holguin <116053539+enrique-holguin@users.noreply.github.com>
2023-12-09 12:22:16 -0500

    Revert "Feat/passport strategy"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import configDB from './ormConfig';
 
 @Module({
-  imports: [UserModule, AuthModule, TypeOrmModule.forRoot(configDB)],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
