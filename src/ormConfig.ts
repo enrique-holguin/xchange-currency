8a89563c43dd1927308c897f394c6eaa78cfa18d
Enrique <e.holguin993@gmail.com>
2023-12-08 23:32:58 -0500

    create currency entities

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,5 +1,6 @@
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 import { User } from './user/entities/user.entity';
+import { Currency } from './common/entities/currency.entity';
 
 const configDB: MysqlConnectionOptions = {
   type: 'mysql',
@@ -8,7 +9,7 @@ const configDB: MysqlConnectionOptions = {
   port: 3306,
   username: 'root',
   password: 'admin',
-  entities: [User],
+  entities: [User, Currency],
   synchronize: true,
 };
 
