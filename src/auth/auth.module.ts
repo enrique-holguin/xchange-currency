9ac698710f41483903e02b792b931fd65300132e
Enrique <e.holguin993@gmail.com>
2023-12-09 11:05:58 -0500

    add login function

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,16 @@ import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
 import { User } from 'src/user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    JwtModule.register({
+      secret: process.env.jwt_secret,
+      signOptions: { expiresIn: '3600s' },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [AuthService, UserService],
   controllers: [AuthController],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
   async validateUser(email: string, password: string) {
     const user = await this.userService.findOne(email);
     if (!user) {
@@ -18,7 +22,17 @@ export class AuthService {
     }
   }
 
-  async login(user:User) {
-    
+  async login(user: User) {
+    const payload = {
+      email: user.email,
+      sub: {
+        name: user.name,
+      },
+    };
+
+    return {
+      ...user,
+      accesToken: this.jwtService.sign(payload),
+    };
   }
 }
