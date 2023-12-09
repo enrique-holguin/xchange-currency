d190d7cf4d09108a715a2becf2065f0644afcf79
Enrique <e.holguin993@gmail.com>
2023-12-09 17:26:46 -0500

    exchange JWTGuard

--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { ExchangeService } from './exchange.service';
+import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('exchange')
-export class ExchangeController {}
+export class ExchangeController {
+  constructor(private readonly exchangeService: ExchangeService) {}
+  @UseGuards(JwtGuard)
+  @Post()
+  calculateExchange() {
+    return 'autorizado';
+  }
+}
