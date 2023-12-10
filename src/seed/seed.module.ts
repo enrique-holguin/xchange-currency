0948d327ea606808033b400c647184c437b2d020
Enrique <e.holguin993@gmail.com>
2023-12-09 20:35:01 -0500

    add SeedModule

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExchangeCurrency } from 'src/exchange/entities/exchange-currency.entity';
+import { Currency } from 'src/exchange/entities/currency.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ExchangeCurrency, Currency])],
   providers: [SeedService],
+  exports: [SeedService],
 })
 export class SeedModule {}
