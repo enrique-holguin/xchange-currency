869056bd40dd840f14e1129019c2963b26992446
Enrique <e.holguin993@gmail.com>
2023-12-11 08:42:05 -0500

    add getAllCurrencies

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -7,12 +7,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ExchangeCurrency } from './entities/exchange-currency.entity';
 import { Repository } from 'typeorm';
 import { ExchangeDto } from './dto/exchange.dto';
+import { Currency } from './entities/currency.entity';
 
 @Injectable()
 export class ExchangeService {
   constructor(
     @InjectRepository(ExchangeCurrency)
     private readonly exchangeCurrencyRepository: Repository<ExchangeCurrency>,
+    @InjectRepository(Currency)
+    private readonly currencyRepository: Repository<Currency>,
   ) {}
   async calculateExchange(data: ExchangeDto) {
     const { from, to, amount } = data;
@@ -34,6 +37,15 @@ export class ExchangeService {
       to: baseCurrency.targetCurrency.base,
       exchangeRate: baseCurrency.exchangeRate,
     };
+  }
+
+  async getAllCurrencies() {
+    try {
+      const currencies = await this.currencyRepository.find();
+      return currencies;
+    } catch (error) {
+      this.handleError(error);
+    }
   }
 
   private handleError(error: any): void {
