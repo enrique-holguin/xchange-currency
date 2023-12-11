f570c18f905f3d504c6f3b035a3df1823e5a029d
Enrique <e.holguin993@gmail.com>
2023-12-11 06:38:49 -0500

    jwt

--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Post, UseGuards, Body } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
 import { JwtGuard } from '../auth/guards/jwt-auth.guard';
 import { ExchangeDto } from './dto/exchange.dto';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Exchange')
 @Controller('exchange')
 export class ExchangeController {
   constructor(private readonly exchangeService: ExchangeService) {}
+  @ApiBearerAuth('jwt')
   @ApiBody({
     description: 'Exchange executed',
     type: ExchangeDto,
