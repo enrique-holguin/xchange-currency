d410ad038a232ff29e52d1080aa6c3cd3754459d
Enrique <e.holguin993@gmail.com>
2023-12-11 08:42:15 -0500

    add getCurrencies

--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Get } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
 import { JwtGuard } from '../auth/guards/jwt-auth.guard';
 import { ExchangeDto } from './dto/exchange.dto';
-import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Currency } from './entities/currency.entity';
 
 @ApiTags('Exchange')
 @Controller('exchange')
@@ -17,6 +23,18 @@ export class ExchangeController {
   @Post()
   async calculateExchange(@Body() exchangeDto: ExchangeDto) {
     const result = await this.exchangeService.calculateExchange(exchangeDto);
+    return result;
+  }
+  @ApiBearerAuth('jwt')
+  @ApiOkResponse({
+    description: 'Retrieve the base and description of the currencies',
+    type: Currency,
+    isArray: true,
+  })
+  @UseGuards(JwtGuard)
+  @Get('list')
+  async getCurrencies() {
+    const result = await this.exchangeService.getAllCurrencies();
     return result;
   }
 }
