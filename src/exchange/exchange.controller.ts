86908ad22ebc25b8e813aade6abbbc32313949f8
Enrique <e.holguin993@gmail.com>
2023-12-15 11:36:45 -0500

    from post to get exchange

--- src/exchange/exchange.controller.ts
+++ src/exchange/exchange.controller.ts
@@ -1,43 +1,25 @@
-import {
-  Controller,
-  Post,
-  UseGuards,
-  Body,
-  Get,
-  Param,
-  Query,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Controller, UseGuards, Get, Query } from '@nestjs/common';
 import { ExchangeService } from './exchange.service';
 import { JwtGuard } from '../auth/guards/jwt-auth.guard';
 import { ExchangeDto } from './dto/exchange.dto';
-import {
-  ApiBearerAuth,
-  ApiBody,
-  ApiOkResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Currency } from './entities/currency.entity';
+import { ExchangeCurrency } from './entities/exchange-currency.entity';
+import { ResponseExchangeDto } from './dto/responseExchange.dto';
 
 @ApiTags('Exchange')
 @Controller('exchange')
 export class ExchangeController {
   constructor(private readonly exchangeService: ExchangeService) {}
   @ApiBearerAuth('jwt')
-  // @ApiBody({
-  //   description: 'Exchange executed',
-  //   type: ExchangeDto,
-  // })
+  @ApiOkResponse({
+    description: 'Retrieve the base and description of the currencies',
+    type: ResponseExchangeDto,
+  })
   @UseGuards(JwtGuard)
   @Get()
   async calculateExchange(
-    @Query(
-      new ValidationPipe({
-        transform: true,
-        transformOptions: { enableImplicitConversion: true },
-        forbidNonWhitelisted: true,
-      }),
-    )
+    @Query()
     query: ExchangeDto,
   ) {
     const result = await this.exchangeService.calculateExchange(query);
