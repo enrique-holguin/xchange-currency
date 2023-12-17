e2404d394d3216249d68444d2d15f0842fa78ca3
Enrique <e.holguin993@gmail.com>
2023-12-17 18:16:54 -0500

    fix seed.controller

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
@@ -11,8 +11,9 @@ export class SeedController {
     status: 200,
     description: 'Seed data inserted successfully',
   })
+  @ApiResponse({ status: 200, description: 'Seed data inserted successfuly' })
   @ApiResponse({ status: 500, description: 'Interal Server Error' })
-  @Post('data')
+  @Get('data')
   async seedData() {
     await this.seedService.seedData();
     return { message: 'Seed data inserted successfully' };
