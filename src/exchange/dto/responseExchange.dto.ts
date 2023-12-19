ddfacd0a15acbb46ef6d00b2531832fcb754e777
Enrique <e.holguin993@gmail.com>
2023-12-19 12:54:25 -0500

    remove example at rate & exchangedAmount

--- src/exchange/dto/responseExchange.dto.ts
+++ src/exchange/dto/responseExchange.dto.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 import { ExchangeDto } from './exchange.dto';
 
 export class ResponseExchangeDto extends ExchangeDto {
-  @ApiProperty({ example: 0.2662 })
+  @ApiProperty()
   exchangeRate: number;
-  @ApiProperty({ example: 5.324 })
+  @ApiProperty()
   exchangedAmount: number;
 }
