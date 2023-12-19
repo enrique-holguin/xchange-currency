af7885afcef1189db29b9fff0d942fc15310f303
Enrique <e.holguin993@gmail.com>
2023-12-19 12:28:39 -0500

    add examples to create-user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -6,19 +6,20 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { user } from '../example/user.example';
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({ example: user.name })
   @MinLength(3)
   @MaxLength(25)
   @IsString()
   name: string;
-  @ApiProperty()
+  @ApiProperty({ example: user.email })
   @IsEmail()
   @MinLength(8)
   @MaxLength(50)
   email: string;
-  @ApiProperty()
+  @ApiProperty({ example: user.password })
   @MinLength(6)
   @MaxLength(50)
   @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
